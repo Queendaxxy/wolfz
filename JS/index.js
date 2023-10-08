55980c95419635a55cf7337ee5eb4fd55a6b61fc
Queendaxxy <queendaxxy@gmail.com>
2023-10-07 17:59:46 -0700

    Add html, CSS and JS files

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+
+function trigger() {
+    var alert=document.getElementById("customAlert");
+    alert.style.display='block';
+}
+function hidealert() {
+    var alert=document.getElementById("customAlert");
+    alert.style.display='none';
+}
+
